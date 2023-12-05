1ab6765fc61950154f2051274d845937843d8046
KirMaid <mk@intervolga.ru>
2023-12-05 12:24:22 +0300

    init

---
+++ .top.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Компания", 
+		"company/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Продукция", 
+		"products/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Услуги",
+		"services/",
+		Array(),
+		Array(),
+		""
+	),
+    Array(
+        "Контакты",
+        "contacts/",
+        Array(),
+        Array(),
+        ""
+    )
+);
+?>
