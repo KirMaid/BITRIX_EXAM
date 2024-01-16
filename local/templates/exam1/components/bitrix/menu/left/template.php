c3398bf030a2bde4c3501b060c18c1b7bcc6135c
KirMaid <mk@intervolga.ru>
2024-01-16 16:52:06 +0300

    Замена цвета в меню по свойству сайта/страницы

--- local/templates/exam1/components/bitrix/menu/left/template.php
+++ local/templates/exam1/components/bitrix/menu/left/template.php
@@ -1,7 +1,7 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?if (!empty($arResult)):?>
-<div class="side-block side-menu">
-	<div class="title-block"><?=GetMessage("T_EXAM_MENU_TITLE")?></div>
+<div class="side-menu side-block">
+	<div class="title-block <? $APPLICATION->ShowProperty('left_menu_color');?>"><?=GetMessage("T_EXAM_MENU_TITLE")?></div>
 	<div class="menu-block">
 		<ul>
 		<?
